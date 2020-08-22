5915d1a4bc840293b5c0c53936e40fef81f9b5eb
Takafumi Arakaki <aka.tkf@gmail.com>
2020-08-22 13:04:56 -0700

    Style tweaks

--- benchmark/bench_prefetch.jl
+++ benchmark/bench_prefetch.jl
@@ -12,7 +12,7 @@ for p in 14:16
     n = 2^p
     xs = [randn(32) for _ in 1:n]
     shuffle!(xs)
-    @assert sum(sum, xs) ≈ sum(sum,  prefetching(xs))
+    @assert sum(sum, xs) ≈ sum(sum, prefetching(xs))
 
     s1 = SUITE[:n=>n] = BenchmarkGroup()
     s1[:impl=>:base] = @benchmarkable sum(sum, $xs)
--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,7 +2,6 @@ using BenchmarkTools
 SUITE = BenchmarkGroup()
 for file in readdir(@__DIR__)
     if startswith(file, "bench_") && endswith(file, ".jl")
-        SUITE[file[length("bench_") + 1:end - length(".jl")]] =
-            include(file)
+        SUITE[file[length("bench_")+1:end-length(".jl")]] = include(file)
     end
 end
