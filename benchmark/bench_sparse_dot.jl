c1c33c0cc090ee9c7345e423e3b40080d6c207a7
Takafumi Arakaki <aka.tkf@gmail.com>
2020-08-22 21:17:48 -0700

    Define `iterate` for `simdeachindex` and use it in benchmark (#17)

--- benchmark/bench_sparse_dot.jl
+++ benchmark/bench_sparse_dot.jl
@@ -5,7 +5,7 @@ using LinearAlgebra: dot
 using LoopRecipes
 using Random: Random
 using SIMD: Vec
-using SparseArrays: SparseVector, sprandn
+using SparseArrays: SparseVector, nonzeroinds, nonzeros, sprandn
 
 Random.seed!(1234)
 
@@ -18,15 +18,29 @@ function simddot(xs::SparseVector, ys::Vector)
     end
 end
 
+function simddot_iter(xs::SparseVector, ys::Vector)
+    vals = nonzeros(xs)
+    inds = nonzeroinds(xs)
+    acc = zero(eltype(xs)) * zero(eltype(ys))
+    @inbounds for k in simdeachindex(inds)
+        x = vals[k]
+        y = ys[inds[k]]
+        z = x * y
+        acc = (z isa Vec ? acc : sum(acc)) + z
+    end
+    return acc
+end
+
 for p in 8:10
     n = 2^p
     xs = sprandn(n, 0.1)
     ys = randn(n)
-    @assert dot(xs, ys) ≈ simddot(xs, ys)
+    @assert dot(xs, ys) ≈ simddot(xs, ys) ≈ simddot_iter(xs, ys)
 
     s1 = SUITE[:n=>n] = BenchmarkGroup()
     s1[:impl=>:base] = @benchmarkable dot($xs, $ys)
     s1[:impl=>:folds] = @benchmarkable simddot($xs, $ys)
+    s1[:impl=>:iter] = @benchmarkable simddot_iter($xs, $ys)
 end
 
 end  # module
